36c926145b80a87bbe7ce5ca0fbef0fcf79bfcfd
augusin11 <augusin11@gmail.com>
2020-05-04 21:27:46 -0700

    Added saveImageFile() method. Work needed to save camera image to local file in full resolution.

--- app/src/main/java/com/example/the_calories_tracking_app/MainActivity.java
+++ app/src/main/java/com/example/the_calories_tracking_app/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.the_calories_tracking_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String photoSaveLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            System.out.println("hello!");
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private File saveImageFile() throws IOException {
+        String name = "JPG_scan_image";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageSaveFile = File.createTempFile(name, ".jpg", storageDir);
+        photoSaveLocation = imageSaveFile.getAbsolutePath();
+        return imageSaveFile;
+    }
+
+
 }
