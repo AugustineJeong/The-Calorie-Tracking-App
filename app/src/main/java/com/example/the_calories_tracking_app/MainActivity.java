46aaae33ffaffb9e5fd88f234785020604dcb79a
augusin11 <augusin11@gmail.com>
2020-05-05 03:00:31 -0700

    Implemented processImage() method in ProgramActivity.java

--- app/src/main/java/com/example/the_calories_tracking_app/MainActivity.java
+++ app/src/main/java/com/example/the_calories_tracking_app/MainActivity.java
@@ -42,14 +42,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
-                Intent imageScreen = new Intent(this, ProgramActivity.class);
-                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                imageScreen.putExtra("image", bitmap);
-                startActivity(imageScreen);
-            }
-        }
+        Intent imageScreen = new Intent(this, ProgramActivity.class);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageScreen.putExtra("image", bitmap);
+        startActivity(imageScreen);
     }
 }
 
--- app/src/main/java/com/example/the_calories_tracking_app/ProgramActivity.java
+++ app/src/main/java/com/example/the_calories_tracking_app/ProgramActivity.java
@@ -1,11 +1,22 @@
 package com.example.the_calories_tracking_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 public class ProgramActivity extends AppCompatActivity {
 
@@ -19,5 +30,26 @@ public class ProgramActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView3);
         imageView.setImageBitmap(bitmap);
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getCloudTextRecognizer();
+
+        Task<FirebaseVisionText> result = processImage(textRecognizer, image);
+    }
+
+    private Task<FirebaseVisionText> processImage(FirebaseVisionTextRecognizer textRecognizer, FirebaseVisionImage image) {
+        Task<FirebaseVisionText> result = textRecognizer.processImage(image).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                backToMain();
+            }
+        });
+        return result;
+    }
+
+    private void backToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
