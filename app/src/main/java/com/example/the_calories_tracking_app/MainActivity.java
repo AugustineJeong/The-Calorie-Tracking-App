01c5bd8d203b99afbf99edcc52b0a5f0a9f923de
augusin11 <augusin11@gmail.com>
2020-05-05 04:51:42 -0700

    Managed to save the image file as a local save instead of using bitmap. Greatly improved image quality!

--- app/src/main/java/com/example/the_calories_tracking_app/MainActivity.java
+++ app/src/main/java/com/example/the_calories_tracking_app/MainActivity.java
@@ -17,9 +17,8 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-// CITATION: takePictureIntent() copied from
-// https://developer.android.com/training/camera/photobasics#java
-// onActivityResult() edited from
+// CITATION: createImageFile() and takePhotoIntent() copied from
+// Google's Android Developers Documentation,
 // https://developer.android.com/training/camera/photobasics#java
 
 
@@ -32,20 +31,46 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void takePictureIntent(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, 1);
-        }
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Intent imageScreen = new Intent(this, ProgramActivity.class);
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        imageScreen.putExtra("image", bitmap);
-        startActivity(imageScreen);
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    public void takePhotoIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.AugustineLabs.calories.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
     }
 }
 
