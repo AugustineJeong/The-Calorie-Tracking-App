03b1dd6709a01d6a82ce6b6f3f95ac7964d35e12
augusin11 <augusin11@gmail.com>
2020-05-05 02:37:17 -0700

    Camera and bitmap is working now, able to use bitmap value of photo.

--- app/src/main/java/com/example/the_calories_tracking_app/ProgramActivity.java
+++ app/src/main/java/com/example/the_calories_tracking_app/ProgramActivity.java
@@ -3,7 +3,9 @@ package com.example.the_calories_tracking_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class ProgramActivity extends AppCompatActivity {
 
@@ -12,8 +14,10 @@ public class ProgramActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program);
 
-//        Bundle bundle = getIntent().getExtras();
-//        Bitmap bitmap = bundle.getParcelable("image");
+        Bundle bundle = getIntent().getExtras();
+        Bitmap bitmap = (Bitmap) bundle.get("image");
 
+        ImageView imageView = findViewById(R.id.imageView3);
+        imageView.setImageBitmap(bitmap);
     }
 }
